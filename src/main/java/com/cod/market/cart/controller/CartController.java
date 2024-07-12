af21ec4eb0cf52ade1987e36a6f61d933050dc1d
taewoo922 <taewoo922@gmail.com>
2024-07-12 15:21:18 +0900

    add cart feature

--- src/main/java/com/cod/market/cart/controller/CartController.java
+++ src/main/java/com/cod/market/cart/controller/CartController.java
@@ -1,21 +1,42 @@
 package com.cod.market.cart.controller;
 
 import com.cod.market.cart.service.CartService;
+import com.cod.market.member.entity.Member;
+import com.cod.market.member.service.MemberService;
+import com.cod.market.product.entity.Product;
+import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
     private final CartService cartService;
+    private final MemberService memberService;
+    private final ProductService productService;
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/list")
     public String list() {
         return "cart/list";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/add/{id}")
+    public String add(@PathVariable("id") Long id, Principal principal) {
+        Product product = productService.getProduct(id);
+        Member member = memberService.findByUserName(principal.getName());
+
+        cartService.add(product, member);
+
+        return "redirect:/cart/list";
+    }
+
+
+
 }
